55a83c68e02f6889793ec036dfae8977e899ea4f
Sy Le <lenguyensy@gmail.com>
2024-04-05 20:12:45 -0700

    0.3.0
    use packages from npm
    
    clean up more paths
    
    Update README.md

---
+++ src/components/ActionDialogs.tsx
@@ -0,0 +1,116 @@
+import { Fragment, ReactNode } from 'react';
+import { useActionDialogs } from './ActionDialogsContext';
+import AlertDialog from './AlertDialog';
+import ChoiceDialog from './ChoiceDialog';
+import ModalDialog from './ModalDialog';
+import PromptDialog from './PromptDialog';
+
+type ActionDialogsProps = {};
+
+export default function ActionDialogs(props: ActionDialogsProps): ReactNode {
+  const { dialogs, dismiss } = useActionDialogs();
+
+  if (!dialogs || dialogs.length === 0) {
+    return null;
+  }
+
+  return (
+    <>
+      {dialogs.map((dialog, idx) => {
+        if (!dialog) {
+          return null;
+        }
+
+        const onConfirmSubmit = () => {
+          dismiss();
+          dialog.onSubmit && dialog.onSubmit(true);
+        };
+        const onPromptSaveClick = (newValue?: string) => {
+          dismiss();
+          dialog.onSubmit && dialog.onSubmit(true, newValue);
+        };
+        const onChoiceSelect = (newValue?: string) => {
+          dismiss();
+          dialog.onSubmit && dialog.onSubmit(true, newValue);
+        };
+        const onDimiss = () => {
+          dismiss();
+          dialog.onSubmit && dialog.onSubmit(false);
+        };
+
+        let contentDom = <></>;
+        switch (dialog.type) {
+          case 'alert':
+            contentDom = (
+              <AlertDialog
+                open={true}
+                title='Alert'
+                message={dialog.message}
+                onDismiss={onDimiss}
+                isConfirm={false}
+              />
+            );
+            break;
+          case 'confirm':
+            contentDom = (
+              <AlertDialog
+                open={true}
+                title='Confirmation'
+                message={dialog.message}
+                yesLabel={dialog.yesLabel}
+                onYesClick={onConfirmSubmit}
+                onDismiss={onDimiss}
+                isConfirm={true}
+              />
+            );
+            break;
+          case 'prompt':
+            contentDom = (
+              <PromptDialog
+                open={true}
+                title={dialog.title}
+                message={dialog.message}
+                value={dialog.value}
+                onSaveClick={onPromptSaveClick}
+                onDismiss={onDimiss}
+                languageMode={dialog.languageMode}
+                isLongPrompt={dialog.isLongPrompt}
+                saveLabel={dialog.saveLabel}
+                required={dialog.required}
+                readonly={dialog.readonly}
+              />
+            );
+            break;
+          case 'choice':
+            contentDom = (
+              <ChoiceDialog
+                open={true}
+                title={dialog.title}
+                message={dialog.message}
+                options={dialog.options}
+                onSelect={onChoiceSelect}
+                onDismiss={onDimiss}
+                required={dialog.required}
+              />
+            );
+            break;
+          case 'modal':
+            contentDom = (
+              <ModalDialog
+                open={true}
+                title={dialog.title}
+                message={dialog.message}
+                onDismiss={onDimiss}
+                showCloseButton={!!dialog.showCloseButton}
+                disableBackdropClick={!!dialog.disableBackdropClick}
+                size={dialog.size}
+              />
+            );
+            break;
+        }
+
+        return <Fragment key={idx}>{contentDom}</Fragment>;
+      })}
+    </>
+  );
+}
--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import {
-  ActionDialogsContext as _ActionDialogsContext,
-  useActionDialogs as _useActionDialogs,
-} from './components/ActionDialogsContext';
-
-export const ActionDialogsContext = _ActionDialogsContext;
-export const useActionDialogs = _useActionDialogs;

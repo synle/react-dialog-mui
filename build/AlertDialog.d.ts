7681f917a4cba7061d4b01b0beb524252d3b2031
Sy Le <lenguyensy@gmail.com>
2024-04-07 15:23:43 -0700

    extract types (#17)

--- build/AlertDialog.d.ts
+++ build/AlertDialog.d.ts
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import { BaseDialogInput } from './ActionDialogsContext';
+import { BaseDialogInput } from './types';
 export type AlertInput = BaseDialogInput & {
     message: ReactNode;
     yesLabel?: ReactNode;
